1847da8849d46717a787cd52eff055d4f2d953f5
ZwEin <zwein27@gmail.com>
2016-10-05 10:42:24 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: ZwEin
+# @Date:   2016-09-30 14:01:47
+# @Last Modified by:   ZwEin
+# @Last Modified time: 2016-10-05 10:40:49
+
+
+from distutils.core import setup
+from setuptools import Extension,find_packages
+from os import path
+
+setup(
+    name = 'digPostingDateExtractor',
+    version = '0.1.0',
+    description = 'digPostingDateExtractor',
+    author = 'Lingzhe Teng',
+    author_email = 'zwein27@gmail.com',
+    url = 'https://github.com/usc-isi-i2/dig-posting-date-extractor',
+    download_url = 'https://github.com/usc-isi-i2/dig-posting-date-extractor',
+    packages = find_packages(),
+    keywords = ['posting', 'date', 'extractor'],
+    install_requires=['digPostingDateExtractor']
+    )
