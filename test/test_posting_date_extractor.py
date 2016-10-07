23e4b4b99987337fff86a7ac800c16e23d50b42e
ZwEin <zwein27@gmail.com>
2016-10-06 20:52:20 -0700

    fix typo

--- test/test_posting_date_extractor.py
+++ test/test_posting_date_extractor.py
@@ -18,10 +18,10 @@ class TestDrugUseExtractorMethods(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_price_extractor(self):
+    def test_posting_extractor(self):
         doc = {'content': 'online: Jul 07, 00:44 \n  ', 'b': 'world'}
 
-        extractor = PostingDateExtractor().set_metadata({'extractor': 'drug'})
+        extractor = PostingDateExtractor().set_metadata({'extractor': 'posting_date'})
         extractor_processor = ExtractorProcessor().set_input_fields(['content']).set_output_field('extracted').set_extractor(extractor)
         updated_doc = extractor_processor.extract(doc)
         self.assertEqual(updated_doc['extracted']['value'], '2016-07-07')
